6293748c7c9b6ce070be4d6a3c64f0d53bbae16e
Nbiba Bedis <bedisnbiba@gmail.com>
2025-12-15 15:44:37 +0100

    0.5.0

---
+++ update-flatpak.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env -S deno -A
+// deno-lint-ignore-file no-import-prefix
+import { parse as parseXML } from "jsr:@libs/xml@7.0.2";
+import { $ } from "jsr:@david/dax@0.43.2";
+import { createTempDirSync } from "jsr:@david/temp@0.1.1";
+
+const [appName, metainfoFile] = Deno.args;
+// parse metainfo
+const metainfo = parseXML(
+  Deno.readTextFileSync(
+    metainfoFile,
+  ),
+  // deno-lint-ignore no-explicit-any
+) as any;
+const version = metainfo.component.releases.release[0]["@version"];
+const oldVersion = metainfo.component.releases.release[1]["@version"];
+
+$.setPrintCommand(true);
+
+if (!Deno.args.includes("--skip-tag")) {
+  // tag it and push
+  await $`git status && git add -A && git commit -m ${version}`;
+  await $`git tag -a ${version} -m ${version}`;
+  await $`git push --follow-tags`;
+}
+
+// download flatpak repo and update
+using tempDir = createTempDirSync();
+await $`git clone git@github.com:flathub/io.github.sigmasd.${appName}.git ${appName}`
+  .cwd(tempDir.path);
+Deno.chdir(`${tempDir.toString()}/${appName}`);
+
+const newSha = await fetch(
+  `https://github.com/sigmaSd/${appName}/archive/refs/tags/${version}.tar.gz`,
+).then(async (res) => {
+  const hashBuffer = await crypto.subtle.digest("SHA-256", await res.bytes());
+  const sha256 = Array.from(new Uint8Array(hashBuffer)).map((b) =>
+    b.toString(16).padStart(2, "0")
+  ).join("");
+  return sha256;
+});
+const oldSha = await fetch(
+  `https://github.com/sigmaSd/${appName}/archive/refs/tags/${oldVersion}.tar.gz`,
+).then(async (res) => {
+  const hashBuffer = await crypto.subtle.digest("SHA-256", await res.bytes());
+  const sha256 = Array.from(new Uint8Array(hashBuffer)).map((b) =>
+    b.toString(16).padStart(2, "0")
+  ).join("");
+  return sha256;
+});
+
+const result = await $`git checkout ${version}`.noThrow();
+if (result.code !== 0) await $`git checkout -b ${version}`;
+
+await Deno.readTextFile(`./io.github.sigmasd.${appName}.yml`)
+  .then((r) => r.replace(`${oldVersion}.tar.gz`, `${version}.tar.gz`))
+  .then((r) => r.replace(oldSha, newSha))
+  .then((r) => Deno.writeTextFile(`./io.github.sigmasd.${appName}.yml`, r));
+
+await $`git add -A && git commit -m ${version} && git push origin ${version}`;
